df59b5766777a50e1d436346fffb6a17c03d0b11
ROUYVPN <99958524+ROUYVPN@users.noreply.github.com>
2022-02-18 16:59:55 +0700

    Add files via upload

---
+++ rouy.php
@@ -0,0 +1,22 @@
+{"Version":"1.1",
+"Url":"https://github.com/ROUYVPN/ROUY-VPN/raw/main/royu.zip" ,
+"Changelog":"ROUY VPN
+รายละเอียดภายในแอป
+TRUE&DTAC&AIS
+TRUE&DTAC&AIS
+ประเทศของเซิร์ฟเวอร์
+TH เซิร์ฟเวอร์
+การอัปเดตต้องมีอินเตอร์เน็ตทุกครั้ง",
+ 
+ 
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
