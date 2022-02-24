d47ed29a02171ecd41a89eb687b4c5b8ec90573e
ROUYVPN <99958524+ROUYVPN@users.noreply.github.com>
2022-02-24 21:32:19 +0700

    Add files via upload

---
+++ ruay.php
@@ -0,0 +1,19 @@
+{"Version":"1.2",
+"Url":"https://github.com/ROUYVPN/ROUY-VPN/raw/main/ruay.zip" ,
+"Changelog":"RUAY VPN
+รายละเอียดภายในแอป
+ใช้งานทั่ใช้งานทั่วไปให้ใช้เซิฟ VIP-CAT
+ดูYoutubeให้ใช้ VIP-INET",
+ 
+
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
