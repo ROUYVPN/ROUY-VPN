987692b8d1d8bb8ccb6852e4463bb122f73b5af0
ROUYVPN <99958524+ROUYVPN@users.noreply.github.com>
2022-11-08 06:14:12 +0700

    Add files via upload

---
+++ ruay.php
@@ -0,0 +1,24 @@
+{"Version":"3.9.4",
+"Url":"https://github.com/ROUYVPN/ROUY-VPN/raw/main/ruay.zip" ,
+"Changelog":"RUAY VPN
+รายละเอียดภายในแอป
+ใช้งานทั่วไปให้ใช้เซิฟ VIP-CAT/SG ทุกไฟล์
+ดูYoutube-เล่นเกมส์ให้ใช้ VIP-INET
+เฉพาะเล่นเกมส์เท่านั้นใช้ VIP-GAME/TOPGAME 
+เพิ่มเซิฟฟรี VIP-Ready-6 เซิฟ
+แก้ไขไฟล์ SG-EURO-20เซิฟ
+แก้ไขไฟล์ SG-F-10 เซิฟ
+แก้ไขไฟล์ SG-HF-10 เซิฟ
+การอัพเดทไฟล์ต้องมีอินเตอร์เน็ตทุกครั้ง
+ฝากให้คะแนนแอปRUAYในเพลย์สโตร์ด้วยครับ",
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
