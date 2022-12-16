aad7360ca25dcc7a13fad2d00509bb083957518d
ROUYVPN <99958524+ROUYVPN@users.noreply.github.com>
2022-12-16 11:37:06 +0700

    Add files via upload

---
+++ ruay.php
@@ -0,0 +1,16 @@
+{"Version":"3.9.5",
+"Url":"https://github.com/ROUYVPN/ROUY-VPN/raw/main/ruay.zip" ,
+"Changelog":"RUAY VPN
+การอัพเดทไฟล์ต้องมีอินเตอร์เน็ตทุกครั้ง
+Thank You Everyone See you again 888",
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
